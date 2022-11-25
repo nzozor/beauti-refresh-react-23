1ef75bac30099ad9465b26755c0285897e266cbb
Nick Zozor <nick.zozor@gmail.com>
2022-11-25 19:13:48 +0000

    add env test

--- pages/treatments/[id].tsx
+++ pages/treatments/[id].tsx
@@ -23,7 +23,7 @@ const TreatmentPage: NextPage<{ treatment: Treatment }> = ({ treatment }) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const response = await fetch(`${host}treatments`);
+  const response = await fetch(`https://cms.beautiskinclinic.com/treatments`);
   let treatments: Treatment[] = await response.json();
   treatments = Array.isArray(treatments) ? treatments : [];
 
@@ -45,7 +45,9 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export const getStaticProps: GetStaticProps = async (context: any) => {
   const id = context.params?.id;
-  const response = await fetch(`${host}treatments?slug=${id}`);
+  const response = await fetch(
+    `https://cms.beautiskinclinic.com/treatments?slug=${id}`
+  );
   let treatment: Treatment[] = await response.json();
   treatment = Array.isArray(treatment) ? treatment : [];
 
