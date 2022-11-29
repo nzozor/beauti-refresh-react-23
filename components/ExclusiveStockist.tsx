4a72153467ac09549571f998eb0da967b3b4b272
Bobby Aleem <78314667+baxsm@users.noreply.github.com>
2022-11-29 21:02:09 +0500

    Exclusive Stockist css added

--- components/ExclusiveStockist.tsx
+++ components/ExclusiveStockist.tsx
@@ -1,55 +1,48 @@
-import React from "react";
 import Image from "next/image";
+import Link from "next/link";
+import LaunchIcon from '@mui/icons-material/Launch';
 
 const ExclusiveStockist: React.FC = () => {
+
+  const imageList = [
+    {
+      imageSource: '/images/Stockist_Alumier.jpg',
+      ref: 'https://www.alumiermd.co.uk/',
+    },
+    {
+      imageSource: '/images/beauti_hayo.jpg',
+      ref: 'https://hayoumethod.com/',
+    },
+    {
+      imageSource: '/images/Stockist_Skinade.jpg',
+      ref: 'https://skinade.com/',
+    },
+  ]
+
   return (
-    <section>
-      <h3>Exclusive Stockist</h3>
-      <h4>Medical grade skincare exclusively available at our clinic</h4>
-      <div className="flex items-center flex-row space-x-4">
-        <div>
-          <a
-            href="https://www.alumiermd.co.uk/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div>
-              <Image
-                className="js-lazy-image"
-                src="/images/Stockist_Alumier.jpg"
-                alt="Alumier | Beauti Skin Clinic London | Oval | Brixton Road | Skin Treatment"
-                width={"100px"}
-                height={"100px"}
-              />
-            </div>
-          </a>
-        </div>
-        <div>
-          <a href="https://hayoumethod.com/" target="_blank" rel="noreferrer">
-            <div>
-              <Image
-                className="js-lazy-image"
-                src="/images/beauti_hayo.jpg"
-                alt="Hayou | Beauti Skin Clinic London | Oval | Brixton Road | Skin Treatment"
-                width={"100px"}
-                height={"100px"}
-              />
-            </div>
-          </a>
-        </div>
-        <div>
-          <a href="https://skinade.com/" target="_blank" rel="noreferrer">
-            <div>
-              <Image
-                className="js-lazy-image"
-                src="/images/Stockist_Skinade.jpg"
-                alt="Skinade | Beauti Skin Clinic London | Oval | Brixton Road | Skin Treatment"
-                width={"100px"}
-                height={"100px"}
-              />
-            </div>
-          </a>
-        </div>
+    <section className="text-center">
+      <h3 className="mb-[30px] mt-[60px] text-[32px] font-[100] text-[#3e3d3c]">Exclusive Stockist</h3>
+      <h4 className="text-[20px] font-[100] leading-[29px] text-[#3e3d3c] mb-[30px]">Medical grade skincare exclusively available at our clinic</h4>
+      <div className="flex place-items-center flex-col gap-[1rem] lg:flex-row justify-center px-[1rem] lg:px-[5rem] xl:px-[10rem] py-4">
+        {
+          imageList.map((item, index) => {
+            return (
+              <a href={item.ref} target="_blank" key={index} rel="noopener noreferrer">
+                <div className='relative duration-300 cursor-pointer exclusiveContainer flex justify-center place-items-center gap-[2rem] lg:max-w-[650px] border-2 border-2-[#c7cbd640]'>
+                  <Image
+                    className="js-lazy-image cursor-pointer"
+                    loading="lazy"
+                    src={item.imageSource}
+                    alt="Skinade | Beauti Skin Clinic London | Oval | Brixton Road | Skin Treatment"
+                    width={500}
+                    height={300}
+                  />
+                  <LaunchIcon className="exclusiveLinkIcon duration-300  hidden absolute top-[10px] right-[10px] text-[#454545] text-[1.5rem] cursor-pointer" />
+                </div>
+              </a>
+            )
+          })
+        }
       </div>
     </section>
   );
