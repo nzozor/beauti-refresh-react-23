85f71f1ba3957c41735209068c8b83f378ee4d67
Bobby Aleem <78314667+baxsm@users.noreply.github.com>
2022-11-29 21:48:04 +0500

    beauti reviews css fix

--- components/BeautiReviews.tsx
+++ components/BeautiReviews.tsx
@@ -22,9 +22,9 @@ const BeautiReviews: React.FC<Props> = ({ reviews }) => {
     <section>
       <h3 className="mt-[50px] mb-0 text-[32px] text-[#3e3d3c] font-[100]">5 Star Reviews</h3>
       <Swiper
-        className="xl:max-w-[1500px] h-full reviewSwiper"
+        className="xl:max-w-[1200px] h-full reviewSwiper"
         pagination={pagination}
-        spaceBetween={20}
+        spaceBetween={0}
         loop={true}
         slidesPerView={1}
         breakpoints={{
@@ -38,10 +38,10 @@ const BeautiReviews: React.FC<Props> = ({ reviews }) => {
       >
         {reviews.map((review: Review, index: number) => (
           <SwiperSlide key={index} className="slideHandle w-full min-h-[380px]">
-            <div className="slideContainer h-[200px] p-[20px] w-[70%] lg:w-[400px] border-[#C7CBD6] border-2 text-center flex flex-col gap-[13px] justify-center place-items-center">
+            <div className="slideContainer h-[200px] p-[20px] w-[70%] lg:w-[85%] border-[#C7CBD6] border-2 text-center flex flex-col gap-[13px] justify-center place-items-center">
               <div className="quote text-[#3e3d3c] text-[13px]">&quot;{review.quote}&quot;</div>
               <div className="name text-[#b0b5c4]">{review.reviewerName}</div>
-              <div className="">
+              <div className="flex justify-center gap-[0.6rem]">
                 <StarIcon className="text-[25px] text-[#c7cbd6]" />
                 <StarIcon className="text-[25px] text-[#c7cbd6]" />
                 <StarIcon className="text-[25px] text-[#c7cbd6]" />
